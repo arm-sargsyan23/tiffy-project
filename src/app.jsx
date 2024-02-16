baef544f5785aa542b446da8af838a8c82479c26
Armen <armenjan2310@gmail.com>
2024-02-16 23:10:30 +0400

    add: UI header with responsivation , add:react-router-dom

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,101 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
 function App() {
+  const [searchInputValue, setSearchInputValue] =
+    useState('');
+
   return (
     <div>
-      <p className="text-xl  bg-secondary font-lora">Hello men</p>
+      {/* hover:translate-y-[100px] transition ease-linear duration-300 */}
+
+      {/* header */}
+      <div className="w-full flex justify-center bg-white border-b-[1px] border-black font-raleway font-normal">
+        <div className="w-[90%] sm:w-[70%] sm:flex sm:justify-between sm:items-center grid grid-rows-2 relative">
+          <div className="w-full lg:max-w-max flex ">
+            <div className="min-w-max py-8">
+              <img
+                className="sm:w-[110px] w-[100px] h-[34px]"
+                src="/media/images/logo.svg"
+                alt="logo"
+              />
+            </div>
+            <div className="w-full flex justify-end items-center lg:hidden sm:mr-[15px]">
+              <div className="w-[35px] h-[35px] flex justify-center items-center p-[9px] border-primary border-[1px] rounded-full">
+                <img
+                  className="w-full h-full"
+                  src="/media/images/burger.svg"
+                  alt="burger"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="lg:flex hidden gap-x-16 text-xs text-primary">
+            <NavLink
+              activeStyle={{
+                fontWeight: 'bold',
+                color: 'red'
+              }}>
+              Главая
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                fontWeight: 'bold',
+                color: 'red'
+              }}>
+              для Женщин
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                fontWeight: 'bold',
+                color: 'red'
+              }}>
+              для Мужчин
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                fontWeight: 'bold',
+                color: 'red'
+              }}>
+              Новинки
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                fontWeight: 'bold',
+                color: 'red'
+              }}>
+              Скидки
+            </NavLink>
+            <NavLink
+              activeStyle={{
+                fontWeight: 'bold',
+                color: 'red'
+              }}>
+              Контакты
+            </NavLink>
+          </div>
+          <div className="flex gap-x-4">
+            <div className="w-full flex items-center relative">
+              <img
+                className="w-[15px] absolute text-primary right-[13px] top-[35px] sm:top-[10px]"
+                src="/media/images/search.svg"
+                alt="search icon"
+              />
+              <input
+                className="sm:w-[200px] w-full border-[1px] block border-black rounded-full text-xs outline-none px-[16px] pr-[30px] py-[6px]"
+                value={searchInputValue}
+                onChange={(e) => {
+                  setSearchInputValue(
+                    e.target.value
+                  );
+                }}
+                type="text"
+                placeholder="Поиск"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,13 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  screens:{
-    sm:"390px",
-    md:"830px",
-    lg: "1410px",
-    xl: '1920px',
-  },
- 
   theme: {
+    screens:{
+      sm:"560px",
+      md:"830px",
+      lg: "1430px",
+      xl: "1920px",
+    },
     extend: {
       colors:{
         secondary: "#168570",
@@ -22,8 +21,8 @@ module.exports = {
       fontSize:{
         xs:["14px", "19.6px"],
         sm: ["16px", "22.4px"],
-        xl: ["50px", "60px"],
-        "2xl": ["80px", "90px"],
+        lg: ["50px", "60px"],
+        xl: ["80px", "90px"],
       }
     },
     fontFamily:{
